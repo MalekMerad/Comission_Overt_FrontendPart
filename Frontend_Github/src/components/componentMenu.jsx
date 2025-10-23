15a17f7b9ee3560c33d65e3d3d2e15c8f71fcea4
Malek Merad <anamalekmerad2003@gmail.com>
2025-10-23 19:39:26 +0100

    Add files via upload

---
+++ Frontend_Github/src/components/componentMenu.jsx
@@ -0,0 +1,10 @@
+// Logo import Components 
+import NewOperation from './OperationsFolder/NewOperation';
+import DisplayOperation from './OperationsFolder/DisplayOperations';
+
+const componentMap  = {
+    "Créer une nouvelle opération" : NewOperation,
+    "Voir la liste des opérations" : DisplayOperation
+}
+
+export default componentMap;
