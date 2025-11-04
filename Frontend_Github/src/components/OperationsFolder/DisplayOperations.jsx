19f5a9a805833f2039cb3ef1267cd5f46a5e9b0d
Malek Merad <anamalekmerad2003@gmail.com>
2025-11-04 12:27:57 +0100

    Delete Frontend_Github directory

--- Frontend_Github/src/components/OperationsFolder/DisplayOperations.jsx
+++
@@ -1,9 +0,0 @@
-function DisplayOperations(){
-    return(
-        <div className="DisplayOperations-container">
-                DisplayOperations
-        </div>
-    )
-}
-
-export default DisplayOperations;
